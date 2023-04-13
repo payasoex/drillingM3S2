43f3e62c781210ac615ecfd027edd17ee04503c3
retamal_f <javier.retamal.fontannaz@gmail.com>
2023-04-13 15:39:42 -0400

    agregar archivos al repositorio

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+const mon = document.getElementById('mon')
+const tue = document.getElementById('mon')
+const wen = document.getElementById('mon')
+const thu = document.getElementById('mon')
+const fri = document.getElementById('mon')
+const sat = document.getElementById('mon')
+const sun = document.getElementById('mon')
+const allDays = document.getElementById('days')
+
+console.log(allDays)
+
+// function convert(day) {
+//     const fahrenheit = day.textContent * 1.8 + 32
+    
+//     return day.textContent = fahrenheit
+    
+// }
+
